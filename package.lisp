f01754a351bc1adacbfd0f915c1421415fdf1bce
Damien John Melksham <Damien.Melksham@gmail.com>
2016-04-21 23:30:16 +1000

    Automatic peasant commit message!

--- package.lisp
+++ package.lisp
@@ -3,10 +3,15 @@
 (defpackage :testy
   (:use #:cl)
   (:export
+   :load-tests
+   :load-test
+   :deregister-test
+   :deregister-tests
    :qtest
    :run-test
    :make-test
    :run-tests
    :all-tests
    :serialise-test
-   :serialise-tests))
+   :serialise-tests
+   :stat-number-tests))
