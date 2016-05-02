84c2d340ac3712e8c2f2864674541c3086275712
Damien John Melksham <Damien.Melksham@gmail.com>
2016-05-02 20:53:40 +1000

    meh commit

--- convenient-test-accessors.lisp
+++ convenient-test-accessors.lisp
@@ -6,35 +6,28 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Get the list of tags from a test
 
 (defun get-tags (test)
-  "Get the list of tags from a test"
+  "Get a list of applicable tags from a test"
 
 )
-
-;;; Add tag/tags to tests
 
 (defun add-tags (test-identifiers tags)
   "Add tag/tags to tests"
   
 )
 
-;;; Remove tag/tags from tests
 
 (defun remove-tags (test-identifiers tags)
   "Remove tag/tags from tests"
 
 )
 
-;;; Get source-code of a test
 
 (defun get-source (test)
-"Get source-code of a test"
+"Get the source-code of a test"
 
 )
-
-;;; Set the sorce code of a test
 
 (defun set-source (test source)
 "Set the source code of a test"
@@ -42,67 +35,71 @@
 )
 
 (defun get-after-source (test)
-  ""
+  "Get the after-function source code of a test"
   
   )
 
 (defun set-after-source (test source)
-  ""
+  "Set the after-function source code of a test"
   
   )
 
 (defun set-after-sources (test-identifiers source)
-  ""
+  "Set the after-function source code of multiple tests"
   
   )
 
-(defun get-before-source (test)
-  ""
+(defun get-before-source (test-name)
+  "Get the before-function source code from a test"
   
   )
 
-(defun set-before-source (test source)
-  ""
+(defun set-before-source (test-name source)
+  "Set the before-function source code of a test"
   
   )
 
 (defun set-before-sources (test-identifiers source)
-  ""
+  "Set the before-function source code of multiple tests"
   
   )
 
+(defun get-names (test-identifiers)
+
+  )
+
 (defun set-name (test new-name)
-  ""
+  "Set/change the name of a test"
   
   )
 
 (defun get-description (test)
-  ""
+  "Get the description of a test"
   
   )
 
 (defun set-description (test description)
-  ""
+  "Set/change the description of a test"
   
   )
 
 (defun get-expectation (test)
-  ""
+  "Get the expectation type of a test"
   
   )
 
 (defun set-expectation (test)
-  ""
+  "Set/change the expectation type of a test"
   
   )
 
 (defun get-run-value (test)
-  ""
+  "Get the latest run-value of a test"
   
   )
 
 (defun get-test-status (test)
-  ""
+  "Get the current status of a test: whether it is T (PASS) or NIL (FAILED)."
   
   )
 
